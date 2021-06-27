84d56320c5d740a28a54260b2b611b3e5c7ab843
kushagra rana <rkushagra777@gmail.com>
2021-06-27 21:30:21 +0530

    styling changes

--- src/HistoryList.js
+++ src/HistoryList.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const HistoryList = ({ history, title}) => {
     return (
         <div className="mail-list">
-            <h2>{ title }</h2>
+            <h2 className="pghd">{ title }</h2>
             {history.map(mail => (
                     <div className="mail-preview" key={mail.id}>
                         <Link to={`history/${mail.id}`}>
